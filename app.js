3a62cf1f5e0b5ed1cc7071c6cf08820c5f0685c9
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-09-06 12:44:35 +0545

    Add middleware and logger

--- app.js
+++ app.js
@@ -4,21 +4,31 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const blogRouter = require('./controllers/blog');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 
 mongoose.set('strictQuery', false);
 
 const mongoUrl = config.URI;
 
+logger.info('connecting to ', mongoUrl);
+
 mongoose
   .connect(mongoUrl)
   .then(() => {
-    console.log('connected to mongoDB');
+    logger.info('connected to mongoDB');
   })
-  .catch((err) => console.error(err.message));
+  .catch((err) => logger.error(err.message));
 
 app.use(cors());
 app.use(express.json());
 
+app.use(middleware.requestLogger);
+
 app.use('/api/blogs', blogRouter);
+
+app.use(middleware.unknownEndpoint);
+
+app.use(middleware.errorHandler);
 
 module.exports = app;
