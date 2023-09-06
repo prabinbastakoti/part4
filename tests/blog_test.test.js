f6de0f03b3dc4a9ba58b8bc747c58803582c3136
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-09-06 22:37:48 +0545

    Test get req

---
+++ tests/blog_test.test.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+
+const api = supertest(app);
+
+const Blog = require('../models/blog');
+
+const initialBlogs = [
+  {
+    title: 'This is title1',
+    author: 'This is author1',
+    url: 'This is URL1',
+    likes: 100,
+  },
+  {
+    title: 'This is title2',
+    author: 'This is author2',
+    url: 'This is URL2',
+    likes: 200,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+}, 100000);
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+}, 100000);
+
+test('returns the correct amount of blog posts', async () => {
+  const response = await api.get('/api/blogs');
+  expect(response.body).toHaveLength(initialBlogs.length);
+}, 100000);
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
