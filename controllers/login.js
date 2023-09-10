9736f0dc675af693baadf4b20681f84d1b781b55
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-09-10 17:08:17 +0545

    fix password authentication

--- controllers/login.js
+++ controllers/login.js
@@ -11,7 +11,9 @@ loginRouter.post('/', async (request, response) => {
   const passwordCorrect =
     user === null ? false : await bcrypt.compare(password, user.passwordHash);
 
-  if (!user && passwordCorrect) {
+  console.log(passwordCorrect);
+
+  if (!(user && passwordCorrect)) {
     return response.status(401).json({ error: 'invalid username or password' });
   }
 
