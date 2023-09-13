2bfef278ddc3585f6a5154476e4f6aaa702cdfc2
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-09-13 14:23:57 +0545

    Add testing server and route/controller

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const config = require('./utils/config');
 const express = require('express');
+
 require('express-async-errors');
+
 const app = express();
+
 const mongoose = require('mongoose');
 const cors = require('cors');
+
 const blogRouter = require('./controllers/blog');
 const userRouter = require('./controllers/user');
 const loginRouter = require('./controllers/login');
+
 const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
+const TestRouter = require('./controllers/test');
 
 mongoose.set('strictQuery', false);
 
@@ -34,6 +40,8 @@ app.use('/api/blogs', blogRouter);
 app.use('/api/user', userRouter);
 
 app.use('/api/login', loginRouter);
+
+app.use('/api/testing', TestRouter);
 
 app.use(middleware.unknownEndpoint);
 
